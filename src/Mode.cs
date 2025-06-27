aa70423b8a48168588b0eba748d686946477ab02
iRstek05 <42379396+bihn05@users.noreply.github.com>
2025-06-27 23:15:27 +0800

    Add files via upload

---
+++ src/Mode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lwssp.Entinies
+{
+    enum ProcessModeNum
+    {
+        processModeNone,
+        processModeInputLines
+    }
+    enum FileModeNum
+    {
+        fileEmpty,
+        fileNotSaved,
+        fileSaved
+    }
+    internal class Mode
+    {
+    }
+}
